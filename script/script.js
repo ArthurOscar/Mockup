ad55701d8ee10ed0bfb6158930092e0c47e0063f
Arthur <arthur_o_soares@estudante.sesisenai.org.br>
2025-05-06 09:10:12 -0300

    UPDATE - Gestão de rotas terminado com ferrovia sul funcional

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
 function togglePasswordVisibility() {
     let senha = document.getElementById("senha_login");
     if (senha.type === "password") {
-      senha.type = "text";
+        senha.type = "text";
     } else {
-      senha.type = "password";
+        senha.type = "password";
     }
 }
 
-function reload(){
+function reload() {
     location.reload()
 }
 
@@ -15,7 +15,7 @@ var imgMenu1 = "../assects/menu_dashboard.png"
 var imgMenu2 = "../assects/menu_dashboard2.png"
 var menuAberto = false;
 
-function trocar_menu(){
+function trocar_menu() {
     document.getElementById('imagem_menu').src = imgMenu2;
     let aux = imgMenu2;
     imgMenu2 = imgMenu1;
@@ -45,9 +45,45 @@ function enviarDados() {
         validacao_login = false
     }
 
-    
+
     if (validacao_login === true) {
         console.log('Validação feita')
-        window.location.href='https://vale.com/pt/trem-de-passageiros'
+        window.location.href = 'https://vale.com/pt/trem-de-passageiros'
     }
 }
+
+var imgFerrovia1 = "../assects/seta_ferrovia.png";
+var imgFerrovia2 = "../assects/seta_ferrovia2.png";
+var ferroviaAberto = false;
+
+function ferroviaSul() {
+    let menu_rotas = document.getElementById('ferrovia_Aberto');
+    let seta = document.getElementById('seta_sul');
+    if (!ferroviaAberto) {
+        document.getElementById('menu_rotas').style.fontSize = '20px';
+        document.getElementById('menu_rotas').style.height = '100px';
+        document.getElementById('menu_rotas').style.justifyContent = 'top';
+        document.querySelector('#numero').style.padding = '5px';
+        document.querySelector('#numero2').style.padding = '5px';
+        document.querySelector('#numero3').style.padding = '5px';
+        seta.src = imgFerrovia2;
+    } else {
+        document.getElementById('menu_rotas').style.height = '0px';
+        document.getElementById('menu_rotas').style.fontSize = '0';
+        document.querySelector('#numero').style.padding = '0';
+        document.querySelector('#numero2').style.padding = '0';
+        document.querySelector('#numero3').style.padding = '0';
+        seta.src = imgFerrovia1;
+    }
+    ferroviaAberto = !ferroviaAberto;
+}
+
+window.onload = () => {
+    ferroviaAberto = false;
+    document.querySelector('#numero').style.padding = '0';
+    document.querySelector('#numero2').style.padding = '0';
+    document.querySelector('#numero3').style.padding = '0';
+    document.getElementById('ferrovia_Aberto').style.fontSize = '0';
+    document.getElementById('menu_rotas').style.height = '0px';
+    document.getElementById('seta_sul').src = imgFerrovia1;
+};
