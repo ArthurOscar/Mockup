19849a04d6bcda0d5a50c04938449211dc61a4ff
ArthurOscar <arthur_o_soares@estudante.sesisenai.org.br>
2025-10-27 09:37:37 -0300

    Classe e função da API

---
+++ src/apiEmail.php
@@ -0,0 +1,20 @@
+<?php
+
+class Email
+{
+    public function EmailVerify($email)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://emailreputation.abstractapi.com/v1/?api_key=8ab3a072073f4018b6c30d31a6345052&email=' . urlencode($email));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        $dado = json_decode($data, true);
+        if (isset($dado["email_deliverability"]["status_detail"]) && $dado["email_deliverability"]["status_detail"] === "valid_email") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
